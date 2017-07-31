f85fff5ca6e8dd30675e32de25284ae3f4e8d1c0
vieven <vileven11@icloud.com>
2017-07-31 13:42:03 +0300

    first time tryed jsx

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-/**
- * Created by Vileven on 31.07.17.
- */
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
